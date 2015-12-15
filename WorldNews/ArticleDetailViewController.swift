3eaa648189d0f89c05b3a47e9685c1262fddf2a2
Christian Cabarrocas <christiancabarrocas@gmail.com>
2015-12-15 21:14:31 +0100

    ArticleDetail wip

---
+++ WorldNews/ArticleDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ArticleDetailViewController.swift
+//  WorldNews
+//
+//  Created by Christian Cabarrocas on 15/12/15.
+//  Copyright © 2015 Wasabilabs. All rights reserved.
+//
+
+import UIKit
+
+class ArticleDetailViewController: UIViewController {
+
+    let article:Article!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
