bb0ba5220e55cdcacf5fc3161f780efc41117548
Christian Cabarrocas <christiancabarrocas@gmail.com>
2016-03-27 10:41:00 +0200

    Deleted Awesome cache - Added Constructor tests

---
+++ WorldNewsTests/ArticleListConstructorTests.swift
@@ -0,0 +1,40 @@
+//
+//  ArticleListConstructorTests.swift
+//  WorldNews
+//
+//  Created by Christian Cabarrocas on 27/03/16.
+//  Copyright © 2016 Wasabilabs. All rights reserved.
+//
+
+import XCTest
+@testable import WorldNews
+
+class ArticleListConstructorTests: XCTestCase {
+    
+    var communicator: Communicator!
+    var constructor: ArticleListConstructor!
+    let expectation = XCTestExpectation()
+    
+    override func setUp() {
+        super.setUp()
+        communicator = Communicator()
+        constructor = ArticleListConstructor(communicator: communicator, apiType: APIType.TopStoriesHome)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testConstructorInit() {
+        XCTAssertTrue(constructor.cellHeight == 80.0)
+        XCTAssertNotNil(constructor.communicator)
+        XCTAssertNotNil(constructor.apiType)
+        XCTAssertTrue(constructor.apiType == APIType.TopStoriesHome)
+    }
+    
+    func testUpdataData() {
+        constructor.updateData { (result) -> Void in
+            XCTAssertNotNil(result)
+        }
+    }
+}
